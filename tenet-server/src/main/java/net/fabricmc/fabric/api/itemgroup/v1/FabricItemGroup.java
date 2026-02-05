db46b98929e189ae0472e167cd940f90c794adfb
wdog5734 <1798643961@qq.com>
2026-02-06 00:27:19 +0800

    implements fabric item group api

---
+++ tenet-server/src/main/java/net/fabricmc/fabric/api/itemgroup/v1/FabricItemGroup.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.itemgroup.v1;
+
+import net.fabricmc.fabric.impl.itemgroup.FabricItemGroupBuilderImpl;
+import net.minecraft.world.item.CreativeModeTab;
+
+/**
+ * Contains a method to create an item group builder.
+ */
+public final class FabricItemGroup {
+	private FabricItemGroup() {
+	}
+
+	/**
+	 * Creates a new builder for {@link net.minecraft.world.item.CreativeModeTab}. Item groups are used to group items in the creative
+	 * inventory.
+	 *
+	 * <p>You must register the newly created {@link CreativeModeTab} to the {@link Registries#ITEM_GROUP} registry.
+	 *
+	 * <p>You must also set a display name by calling {@link ItemGroup.Builder#displayName(Text)}
+	 *
+	 * <p>Example:
+	 *
+	 * <pre>{@code
+	 * private static final RegistryKey<ItemGroup> ITEM_GROUP = RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of(MOD_ID, "test_group"));
+	 *
+	 * @Override
+	 * public void onInitialize() {
+	 *    Registry.register(Registries.ITEM_GROUP, ITEM_GROUP, FabricItemGroup.builder()
+	 *       .displayName(Text.translatable("modid.test_group"))
+	 *       .icon(() -> new ItemStack(Items.DIAMOND))
+	 *       .entries((context, entries) -> {
+	 *          entries.add(TEST_ITEM);
+	 *       })
+	 *       .build()
+	 *    );
+	 * }
+	 * }</pre>
+	 *
+	 * @return a new {@link ItemGroup.Builder} instance
+	 */
+	public static CreativeModeTab.Builder builder() {
+		return new FabricItemGroupBuilderImpl();
+	}
+}
