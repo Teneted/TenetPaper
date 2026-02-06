cc439bb071603ad857928d6b55f06019534e9bc1
wdog5734 <1798643961@qq.com>
2026-02-06 10:42:12 +0800

    implements fabric-events-interaction-v0 api

--- tenet-server/src/main/java/org/teneted/tenet/TenetMC.java
+++ tenet-server/src/main/java/org/teneted/tenet/TenetMC.java
@@ -1,5 +1,6 @@
 package org.teneted.tenet;
 
+import net.fabricmc.fabric.impl.event.interaction.InteractionEventsRouter;
 import net.fabricmc.fabric.impl.tag.convention.v2.TranslationConventionLogWarnings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ public class TenetMC {
 
     public static void init() {
         LOGGER.info("TenetModLoader is loading...");
+        InteractionEventsRouter.onInitialize();
         TranslationConventionLogWarnings.onInitialize();
     }
 }
